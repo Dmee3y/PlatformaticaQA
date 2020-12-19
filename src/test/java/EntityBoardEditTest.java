03e457f4605bc5f9efc6a78e2f2b296ab5c947cb
irina0606 <irzhik1975@gmail.com>
2020-12-18 19:47:51 -0500

    111

--- src/test/java/EntityBoardEditTest.java
+++ src/test/java/EntityBoardEditTest.java
@@ -1,67 +1,60 @@
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+import runner.ProjectUtils;
+import runner.type.Profile;
+import runner.type.ProfileType;
 
 import java.awt.*;
 
-public class BoardEditTest extends BaseTest {
+import static runner.ProjectUtils.click;
+
+public class EntityBoardEditTest extends BaseTest {
+
     @Test
     public void editBoard() throws InterruptedException {
         WebDriver driver = getDriver();
-        driver.get("https://ref.eteam.work/index.php?action=login");
-        //log in
-        ProjectUtils.login(driver,"user3@tester.com", "SXGJhNd1aM");
         WebElement board = driver.findElement(By.xpath("//p[contains(text(),'Board')]"));
         ProjectUtils.click(driver, board);
-
-        // create new values on board and save
 
         driver.findElement(By.xpath("//i[contains(text(),'create_new_folder')]")).click();
         driver.findElement(By.xpath("//textarea[@id='text']")).sendKeys("my test");
         driver.findElement(By.xpath("//input[@id='int']")).sendKeys(String.valueOf(20));
         driver.findElement(By.xpath("//input[@id='decimal']")).sendKeys(String.valueOf(22.5));
 
-
-        /*WebElement calendar1 = driver.findElement(By.xpath("//input[@id='date']"));
-        calendar1.clear();
-        driver.findElement(By.xpath("//input[@id='date']")).sendKeys("15/12/2020");
-        WebElement calendar2 = driver.findElement(By.xpath("//input[@id='datetime']"));
-        calendar2.clear();
-        driver.findElement(By.xpath("//input[@id='datetime']")).sendKeys("20/12/2020 12:40:00");*/
-        //driver.findElement(By.xpath("//input[@id='datetime']")).sendKeys(Keys.ESCAPE);
-        //Thread.sleep(2000);
         ProjectUtils.click(driver, driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")));
-        //driver.findElement(By.id("pa-entity-form-save-btn")).click();
         Thread.sleep(3000);
 
-        //String boardContainer = driver.findElement(By.xpath("//div[@class='bootstrap-table bootstrap4']")).getText();
-        //assertThat(boardContainer).
         driver.findElement(By.xpath("//ul[@class='pa-nav-pills-small nav nav-pills nav-pills-primary']//i[text()='dashboard']")).click();
         driver.findElement(By.xpath("//ul[@class='pa-nav-pills-small nav nav-pills nav-pills-primary']//i[text()='list']")).click();
-        //driver.findElement(By.xpath("//tbody/tr[4]/td[10]/div[1]/button[1]/i[1]")).click();
         driver.findElement(By.xpath("//tbody/tr[1]/td[10]/div[1]/button[1]")).click();
-        //driver.findElement(By.xpath("//tbody/tr[1]/td[10]/div[1]/ul[1]/li[2]/a[1]")).click();
-        ProjectUtils.click(driver, driver.findElement(By.xpath("//tbody/tr[1]/td[10]/div[1]/ul[1]/li[2]/a[1]")));
-        driver.findElement(By.xpath("//div[@class='filter-option-inner-inner'][text()='Pending']")).click();
+        ProjectUtils.click(driver, driver.findElement(By.xpath("//a[text()='edit']")));
+        driver.findElement(By.xpath("//div[contains(text(),'Pending')]")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//span[contains(text(),'On track')]")).click();
-
+        Thread.sleep(2000);
         WebElement text = driver.findElement(By.xpath("//textarea[@id='text']"));
         text.clear();
         driver.findElement(By.xpath("//textarea[@id='text']")).sendKeys("my test changed");
         Thread.sleep(2000);
         ProjectUtils.click(driver, driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']")));
-        // validate edition
+
         String result = driver.findElement(By.xpath("//tbody/tr[1]/td[3]/a[1]/div[1]")).getText();
-        Assert.assertEquals(result,"my test changed");
+        Assert.assertEquals(result, "my test changed");
+        Thread.sleep(2000);
 
+        driver.findElement(By.xpath("//tbody/tr[1]/td[10]/div[1]/button[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[text()='delete']")).click();
 
-
-
-
-
-
-
-
+        driver.findElement(By.xpath("//ul[@class='pa-nav-pills-small nav nav-pills nav-pills-primary']//i[text()='list']")).click();
+        boolean emptyField = driver.findElements(By.xpath("//tbody/tr[1]/td[10]/div[1]/button[1]")).size() < 1;
+        Assert.assertTrue(emptyField);
     }
 }
+
+
